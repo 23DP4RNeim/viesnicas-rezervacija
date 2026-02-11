1e253676b383224f4566362f923e0326bcb029fe
Roberts Neimanis <A230863RN@rvt.lv>
2026-02-11 14:21:30 +0200

    pirmais commits

---
+++ frontend/vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-card class="pa-5">
+          <v-card-title>
+            Vuetify strādā ✅
+          </v-card-title>
+
+          <v-card-text>
+            Viesnīcas rezervēšanas sistēma
+          </v-card-text>
+
+          <v-btn color="primary">
+            Rezervēt numuru
+          </v-btn>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+
+<style scoped></style>
